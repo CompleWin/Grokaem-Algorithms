d7ea6180c846c92275864cfc2fae63c48a5bd802
Nikolai Ivakhnenko <complewin@yandex.ru>
2025-09-30 20:15:48 +0300

    fix grokaem

---
+++ Grokaem_Algo/Algorithm.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Numerics;
+
+namespace Grokaem_Algo;
+
+public class Algorithm
+{
+    #region Chapter 1 BinarySearch
+
+    public static int? BinarySearch<T>(IList<T> collection, T target) where T : INumber<T>
+    {
+        int low = 0;
+        int high = collection.Count - 1;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            T guess = collection[mid];
+            if (guess == target)
+            {
+                return mid;
+            }
+            else if (guess > target)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return null;
+    }
+
+    public static int FindSmallest<T>(IList<T> collection) where T : INumber<T>
+    {
+        T smallest = collection[0];
+        int smallestIndex = 0;
+        for (int i = 0; i < collection.Count; i++)
+        {
+            if (collection[i] < smallest)
+            {
+                smallest = collection[i];
+                smallestIndex = i;
+            }
+        }
+
+        return smallestIndex;
+    }
+
+    #endregion
+
+    #region Chapter 2 SelectionSort
+
+    public static List<T> SelectionSort<T>(IList<T> collection) where T : INumber<T>
+    {
+        List<T> newList = [];
+
+        for (int i = 0; i < collection.Count; i++)
+        {
+            int indexSmallest = FindSmallest<T>(collection);
+            T item = collection[indexSmallest];
+            collection.RemoveAt(indexSmallest);
+            newList.Add(item);
+        }
+
+        return newList;
+    }
+
+    #endregion
+
+    #region Chapter 4 Recursive
+
+    public static T SumRecursive<T>(IList<T> collection) where T : INumber<T>
+    {
+        if (collection.Count == 0)
+        {
+            return T.Zero;
+        }
+
+        if (collection.Count == 1)
+        {
+            return collection[0];
+        }
+
+        return collection[0] + SumRecursive<T>(collection.Skip(1).ToList());
+    }
+
+    public static T MaxRecursive<T>(IList<T> collection) where T : INumber<T>
+    {
+        if (collection.Count == 2)
+        {
+            return collection[0] > collection[1] ? collection[0] : collection[1];
+        }
+
+        T subMax = MaxRecursive<T>(collection.Skip(1).ToList());
+        return collection[0] > subMax ? collection[0] : subMax;
+
+    }
+
+    #endregion
+
+    #region Chapter 4 QuickSort
+
+    public static List<T> QuickSort<T>(IList<T> collection) where T : INumber<T>
+    {
+        if (collection.Count < 2)
+        {
+            return collection.ToList();
+        }
+
+        T pivot = collection[Random.Shared.Next(0, collection.Count)];
+
+        List<T> less = new List<T>();
+        List<T> greater = new List<T>();
+        List<T> center = new List<T>();
+
+        foreach (var element in collection)
+        {
+            if (element == pivot)
+            {
+                center.Add(element);
+            }
+            else if (element > pivot)
+            {
+                greater.Add(element);
+            }
+            else
+            {
+                less.Add(element);
+            }
+        }
+
+        return QuickSort<T>(less).Concat(center).Concat(QuickSort(greater)).ToList();
+    }
+
+    #endregion
+
+    #region Chapter 9 Deykstra
+
+    public static int Deykstra(Dictionary<string, Dictionary<string, int>> graph, Dictionary<string, int> costs,
+        Dictionary<string, string> parents)
+    {
+        List<string> processed = new List<string>();
+        string node = FindLowestNode(costs, processed);
+
+        while (node != null)
+        {
+            int cost = costs[node];
+            var neighbors = graph[node];
+
+            foreach (var n in neighbors)
+            {
+                int new_cost = cost + neighbors[n.Key];
+                if (costs[n.Key] > new_cost)
+                {
+                    costs[n.Key] = new_cost;
+                    parents[n.Key] = node;
+                }
+            }
+
+            processed.Add(node);
+            node = FindLowestNode(costs, processed);
+        }
+
+        return costs["конец"];
+    }
+
+    public static string FindLowestNode(Dictionary<string, int> costs, List<string> proccessed)
+    {
+        int lowestCost = int.MaxValue;
+        string lowestNode = null;
+
+        foreach (var node in costs)
+        {
+            if (node.Value < lowestCost && !proccessed.Contains(node.Key))
+            {
+                lowestNode = node.Key;
+                lowestCost = node.Value;
+            }
+        }
+
+        return lowestNode;
+    }
+
+    #endregion
+
+    #region Greedy
+
+    public static HashSet<string> GreedyAlgo(HashSet<string> statesNeeded, Dictionary<string, HashSet<string>> stations)
+    {
+        HashSet<string> finalStations = new HashSet<string>();
+        
+        while (statesNeeded.Count > 0)
+        {
+            string bestStation = string.Empty;
+            HashSet<string> statesCovered = new HashSet<string>();
+            foreach (var node in stations)
+            {
+                string station = node.Key;
+                HashSet<string> states = node.Value;
+                HashSet<string> covered = statesNeeded.Intersect(states).ToHashSet();
+                if (covered.Count > statesCovered.Count)
+                {
+                    bestStation = station;
+                    statesCovered = covered;
+                }
+            }
+
+            statesNeeded.ExceptWith(statesCovered);
+            finalStations.Add(bestStation);
+        }
+        return finalStations;
+    }
+
+#endregion
+}
---
+++ Grokaem_Algo/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Text;
+
+namespace Grokaem_Algo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+
+        #region Graphs
+
+        // Dictionary<string, Dictionary<string, int>> graph = new Dictionary<string, Dictionary<string, int>>();
+        // graph["начало"] = new Dictionary<string, int>();
+        // graph["начало"]["a"] = 6;
+        // graph["начало"]["b"] = 2;
+        //
+        // graph["a"] = new Dictionary<string, int>();
+        // graph["a"]["конец"] = 1;
+        //
+        // graph["b"] =  new Dictionary<string, int>();
+        // graph["b"]["a"] = 3;
+        // graph["b"]["конец"] = 5;
+        //
+        // graph["конец"] = new Dictionary<string, int>();
+        //
+        // Dictionary<string, int> costs = new Dictionary<string, int>();
+        //
+        // costs["a"] = 6;
+        // costs["b"] = 2;
+        // costs["конец"] = int.MaxValue;
+        //
+        // Dictionary<string, string> parents = new Dictionary<string, string>();
+        //
+        // parents["a"] = "начало";
+        // parents["b"] = "начало";
+        // parents["in"] = String.Empty;
+        //
+        // List<string> processed =  new List<string>();
+        //
+        // Dictionary<string, Dictionary<string, int>> graph2 = new Dictionary<string, Dictionary<string, int>>();
+        // graph2["начало"] = new Dictionary<string, int>()
+        // {
+        //     { "a", 5 },
+        //     { "b", 2 }
+        // };
+        // graph2["a"] = new Dictionary<string, int>()
+        // {
+        //     { "d", 2 },
+        //     { "c", 4 }
+        // };
+        // graph2["b"] = new Dictionary<string, int>()
+        // {
+        //     { "a", 8 },
+        //     { "d", 7 }
+        // };
+        // graph2["c"] = new Dictionary<string, int>()
+        // {
+        //     { "d", 6 },
+        //     { "конец", 3 }
+        // };
+        // graph2["d"] = new Dictionary<string, int>()
+        // {
+        //     { "конец", 1 }
+        // };
+        //
+        // graph2["конец"] = new Dictionary<string, int>();
+        //
+        // Dictionary<string, int> costs2 = new Dictionary<string, int>()
+        // {
+        //     { "a", 5 },
+        //     { "b", 2 },
+        //     { "c", int.MaxValue},
+        //     {"d", int.MaxValue},
+        //     { "конец", int.MaxValue }
+        // };
+        //
+        // Dictionary<string, string> parents2 = new Dictionary<string, string>()
+        // {
+        //     { "a", "начало" },
+        //     { "b", "начало" },
+        //     {"c", null},
+        //     {"d", null},
+        //     {"конец", null}
+        // };
+        //
+        // Console.WriteLine(Algorithm.Deykstra(graph2, costs2, parents2));
+
+        #endregion
+
+        #region Greedy
+
+        HashSet<string> statesNeeded = new HashSet<string>()
+        {
+            "mt", "wa", "or", "id", "nv", "ut", "ca", "az"
+        };
+
+        Dictionary<string, HashSet<string>> stations = new Dictionary<string, HashSet<string>>()
+        {
+            {"kone", new HashSet<string> {"id", "nv", "ut"}},
+            {"ktwo", new HashSet<string> {"wa", "id", "mt"}},
+            {"kthree", new HashSet<string> {"or", "nv", "ca"}},
+            {"kfour", new HashSet<string> {"nv", "ut"}},
+            {"kfive", new HashSet<string> {"ca", "az"}}
+        };
+
+        HashSet<string> answer = Algorithm.GreedyAlgo(statesNeeded, stations);
+
+        foreach (string state in answer)
+        {
+            Console.WriteLine(state);
+        }
+        
+        #endregion
+        
+    }
+}
